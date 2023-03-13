879ae5c0eb9a7b55699bcb935d754db28f72bbe4
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-14 00:47:52 +0100

    Add fields and constructor

--- src/homework/Drink.java
+++ src/homework/Drink.java
@@ -9,4 +9,23 @@ public class Drink extends Food {
 //     для негазированного - "скр",
 //     а для алкогольного сначала спрашивает возраст и издаёт звук, только если возраст больше 18.
 
+  enum Gas {
+    YES,
+    NO
+  }
+
+  enum Alcohol {
+    YES,
+    NO
+  }
+
+  private Gas gas;
+  private Alcohol alcohol;
+
+  public Drink(String name, double calories, Gas gas, Alcohol alcohol) {
+    super(name, calories);
+    this.gas = gas;
+    this.alcohol = alcohol;
+  }
+
 }
